bad34779e3bb0980940b448d396287e1b1417e8f
AlexisDevjs <alexisdevjs@gmail.com>
2024-08-10 18:54:23 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import "@/app/ui/global.css";
+import { inter } from "./ui/fonts";
+
+function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
+
+export default RootLayout;
